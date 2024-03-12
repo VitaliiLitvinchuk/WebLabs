6d2a89d682c35c356347c4dfd319df0ea8cce449
Vitalik-Litvinchuk <61794076+Vitalik-Litvinchuk@users.noreply.github.com>
2024-03-12 17:19:34 +0200

    Added some interactivity on Lab 5

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,6 +1,8 @@
 import classNames from "classnames";
 import "./style.css";
 import { ISkipTrace } from "./types";
+import { useRef, useState } from "react";
+import { useDownloadExcel } from "react-export-table-to-excel";
 
 const people: ISkipTrace[] = [];
 
@@ -27,29 +29,41 @@ const addToPeople = () => {
     });
 }
 
+addToPeople();
+addToPeople();
+addToPeople();
+
 export const Lab5Table = () => {
-    addToPeople();
-    addToPeople();
-    addToPeople();
+    const [showTableRows, SetShowTableRows] = useState<boolean>(true);
+    const [showContent, SetShowContent] = useState<boolean>(true);
+
+    const tableRef = useRef<HTMLTableElement>(null);
+
+    const { onDownload } = useDownloadExcel({
+        currentTableRef: tableRef.current,
+        filename: 'Skiptrace Detail',
+        sheet: 'Detail'
+    });
+
     return (
         <div className="cust-container">
-            <div className="func-icons">
-                <i className="fa fa-close text-light-gray icon-close"></i>
+            <div className="func-icons" onClick={() => SetShowContent(!showContent)}>
+                <i className="fa fa-close text-light-gray icon-close clickable" ></i>
             </div>
-            <div className="table-content">
+            <div className={classNames("table-content", showContent && "display-none")}>
                 <h2><b>Skiptrace Detail</b></h2>
                 <div className="func-icons">
-                    <div className="centered-icons">
-                        <i className="fa fa-2x fa-check-square table-icon"></i>
+                    <div className="centered-icons clickable" onClick={() => SetShowTableRows(!showTableRows)}>
+                        <i className={classNames("fa fa-2x", showTableRows ? "table-icon fa-check-square" : "text-red fa-close")}></i>
                         <span className="text-light-gray">Show / Hide Rows</span>
                     </div>
-                    <div className="centered-icons">
+                    <div className="centered-icons clickable" onClick={onDownload}>
                         <i className="fa fa-2x fa-share-square table-icon"></i>
                         <span className="text-light-gray">Export</span>
                     </div>
                 </div>
                 <div className="ow-auto">
-                    <table>
+                    <table ref={tableRef}>
                         <thead>
                             <tr className="text-light-gray">
                                 <th>Name</th>
@@ -61,20 +75,23 @@ export const Lab5Table = () => {
                                 <th>Race</th>
                             </tr>
                         </thead>
-                        <tbody>
-                            {
-                                people.map(val =>
-                                    <tr>
-                                        <td>{val.name}</td>
-                                        <td>{val.confirmedHomeOwner ? "Yes" : "No"}</td>
-                                        <td className={classNames(val.validPhone && "text-red")}>{val.mobilePhone}</td>
-                                        <td className="text-light-gray">{val.homePhone} &#160;<i className="fa fa-plus table-icon"></i></td>
-                                        <td className="text-light-gray">{val.email} &#160;<i className="fa fa-plus table-icon"></i></td>
-                                        <td>{val.language}</td>
-                                        <td>{val.race || "Unknown"}</td>
-                                    </tr>)
-                            }
-                        </tbody>
+                        {
+                            showTableRows &&
+                            <tbody>
+                                {
+                                    people.map(val =>
+                                        <tr>
+                                            <td>{val.name}</td>
+                                            <td>{val.confirmedHomeOwner ? "Yes" : "No"}</td>
+                                            <td className={classNames(val.validPhone && "text-red")}>{val.mobilePhone}</td>
+                                            <td className="text-light-gray">{val.homePhone} &#160;<i className="fa fa-plus table-icon"></i></td>
+                                            <td className="text-light-gray">{val.email} &#160;<i className="fa fa-plus table-icon"></i></td>
+                                            <td>{val.language}</td>
+                                            <td>{val.race || "Unknown"}</td>
+                                        </tr>)
+                                }
+                            </tbody>
+                        }
                     </table>
                 </div>
             </div>
